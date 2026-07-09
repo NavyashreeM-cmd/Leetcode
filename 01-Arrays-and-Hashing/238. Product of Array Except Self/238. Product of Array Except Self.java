8e6d0dc7725cc41f245c488696c3f6fb0909acbf
Navyashree M <navyashreem181@gmail.com>
2026-07-09 11:37:22 +0530

    [238] [Time Beats: 94.15%] [Memory Beats: 40.50%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/238. Product of Array Except Self/238. Product of Array Except Self.java
@@ -0,0 +1,20 @@
+1class Solution {
+2    public int[] productExceptSelf(int[] nums) {
+3        int n=nums.length;
+4        int[] res=new int[nums.length];
+5
+6        res[0]=1;
+7
+8        for(int i=1;i<n;i++){
+9            res[i]=res[i-1]*nums[i-1];
+10        }
+11        int suffix=1;
+12
+13        for(int i=n-1;i>=0;i--){
+14            res[i]*=suffix;
+15            suffix*=nums[i];
+16        }
+17       return res;
+18
+19    }
+20}
